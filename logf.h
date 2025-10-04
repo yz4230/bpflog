c9e345a90ecffc8fb18522863fc826e446334912
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-10-04 16:54:26 +0900

    fix: correct logf macro parameters and variable names for consistency

--- logf.h
+++ logf.h
@@ -16,7 +16,7 @@ struct {
 // logf outputs logs to user space
 // Note1: Arguments must be cast to u64 type
 // Note2: Maximum log length is 256 bytes
-#define logf(fmt, args...)                                                    \
+#define logf(ctx, fmt, args...)                                                    \
   ({                                                                           \
     static const char _fmt[] = fmt;                                            \
     static char _buf[256];                                                     \
@@ -24,7 +24,7 @@ struct {
     ___bpf_fill(_args, args);                                                  \
     int _len = bpf_snprintf(_buf, sizeof(_buf), _fmt, _args, sizeof(_args));   \
     if (_len < sizeof(_buf)) {                                                 \
-      bpf_perf_event_output(skb, &log_entries, BPF_F_CURRENT_CPU, _buf, _len); \
+      bpf_perf_event_output(ctx, &logf_entries, BPF_F_CURRENT_CPU, _buf, _len); \
     }                                                                          \
   })
 
